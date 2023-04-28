cee4c47991eb8af0a51ef075c4d81df4ffdc76f8
Cesar Fontalvo <juniorfontalvo5@gmail.com>
2023-04-28 16:12:55 -0500

    maquetando la pagina

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,22 @@
+import { Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import { Navbar, SideBar } from "../components";
+
+const drawerWith = 240;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Navbar drawerWith={drawerWith} />
+
+      <SideBar drawerWith={drawerWith} />
+      {/* Sidebar drawerWith */}
+
+      <Box component="main" sx={{ flexGrow: 1, p: 2}}>
+        {/* Toolbar */}
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,35 @@
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView, NoteView } from "../view";
+import { AddOutlined } from "@mui/icons-material";
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>journalPages</Typography>
-    </>
+    <JournalLayout>
+      {/* <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aperiam voluptatibus quis, ratione provident cum tenetur dicta asperiores a quisquam laborum tempore sint similique. Voluptatum aliquid laborum vel recusandae quisquam.</Typography> */}
+    
+      {/* Nothing Selected */}
+      <NothingSelectedView />
+
+      {/* Not Viewed */}
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
+    </JournalLayout>
 
   );
 };
